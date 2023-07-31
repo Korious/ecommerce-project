e17c841ebd020f53e6696f39632bebe3f278ca7b
korious <innovativepotencial@gmail.com>
2023-07-31 12:43:47 +0100

    search

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,18 +31,40 @@ document.addEventListener('DOMContentLoaded', () => {
   const categoryRadios = document.querySelectorAll('[name="tabset"]');
 
   const filterProducts = (category) => {
-    allProducts.forEach(product => product.style.display = category === 'All' || product.classList.contains(category) ? 'list' : 'none');
+    allProducts.forEach(product => product.style.display = category === 'All' || product.classList.contains(category) ? 'block' : 'none');
   };
 
   categoryRadios.forEach(radio => radio.addEventListener('change', () => filterProducts(radio.value)));
 });
 
-
 // ==================================
 // 3. Display products based on 
 // search keywords in the input fields.
 // ==================================
- 
+  document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.querySelector('.search_product');
+    const allProducts = document.querySelectorAll('.catalogue');
+
+    // Function to filter products based on search keywords
+    const filterProductsBySearch = (searchTerm) => {
+      const regex = new RegExp(searchTerm, 'b'); // 'i' flag for case-insensitive search
+      allProducts.forEach((product) => {
+        const productTitle = product.querySelector('p').textContent;
+        if (productTitle.match(regex)) {
+          product.style.display = 'block'; // Show the product if it matches the search term
+        } else {
+          product.style.display = 'none'; // Hide the product if it doesn't match the search term
+        }
+      });
+    };
+
+    // Add event listener to search input
+    searchInput.addEventListener('input', () => {
+      const searchTerm = searchInput.value.trim(); // Get the entered search term and remove leading/trailing spaces
+      filterProductsBySearch(searchTerm);
+    });
+  });
+
 
 
 
