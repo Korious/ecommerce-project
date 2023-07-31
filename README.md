361a7943d56f1c4c707c3f8d123ab0c6bffbbdd0
korious <innovativepotencial@gmail.com>
2023-07-31 11:32:52 +0100

    readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# eCommerce Product Catalogue
+# ECommerce Product Catalogue
 
-In this project, you will be asked to add interactivity to an existing web page. You will use javascript to toggle hamburger menu on mobile screens, display products based on category selected and display product items that matches the user search input field. 
+- In this project, worked with Javascript to bring functionalities to some specifics of this page 
+
+
+**Instructions**
+
+- Use javascript to toggle hamburger menu on mobile screens, display products based on category selected and display product items that matches the user search input field. 
+
+![Image of Page on Desktop](image.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,55 +3,25 @@
 // By default, the .link tag should 
 // display none.
 // ==================================
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('nav ul');
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerButton = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.link');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
+  hamburgerButton.addEventListener('click', function () {
+    navLinks.style.display = navLinks.style.display === 'none' ? 'block' : 'none';
+  });
 });
-
 // ==================================
 // 2. Display products based on 
 // All, Men or Female categories.
 // ==================================
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Display products based on selected category
-  const categories = document.querySelectorAll('.category');
-  const productItems = document.querySelectorAll('.percard.catalogue');
 
-  categories.forEach(category => {
-    category.addEventListener('click', () => {
-      const selectedCategory = category.getAttribute('data-category');
-
-      productItems.forEach(item => {
-        const itemCategory = item.getAttribute('data-category');
-
-        if (selectedCategory === 'All' || selectedCategory === itemCategory) {
-          item.style.display = 'block';
-        } else {
-          item.style.display = 'none';
-        }
-      });
-    });
-  });
 // ==================================
 // 3. Display products based on 
 // search keywords in the input fields.
 // ==================================
-const searchInput = document.querySelector('.search_product');
+ 
 
-searchInput.addEventListener('input', () => {
-  const searchTerm = searchInput.value.toLowerCase();
 
-  productItems.forEach(item => {
-    const itemName = item.querySelector('p').textContent.toLowerCase();
 
-    if (itemName.includes(searchTerm)) {
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-});
-});
